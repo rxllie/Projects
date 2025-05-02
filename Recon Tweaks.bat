f485cf96318227617f7d0af2bccfff24b906d9e8
rxllie <136789601+rxllie@users.noreply.github.com>
2025-05-02 21:41:00 +0300

    Update Recon Tweaks.bat

--- Recon Tweaks.bat
+++ Recon Tweaks.bat
@@ -1,4 +1,4 @@
 @echo off
 chcp 437 >nul 2>&1
-powershell "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Recon is now Vone, Press "OK" To open Vone's website. ', 'Vone', 'Ok', [System.Windows.Forms.MessageBoxIcon]::Error);}"
+powershell "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Recon is now Vone, Press "OK" To open Vone's website. ', 'Vone', 'Ok', [System.Windows.Forms.MessageBoxIcon]::Warning);}"
 chcp 65001 >nul 2>&1
